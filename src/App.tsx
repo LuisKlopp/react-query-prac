35efe10078c2b112f93bd74ca524ad2664bf9bb3
LuisKlopp <fbgus333@gmail.com>
2022-10-24 18:20:18 +0900

    feat:reactquery

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, QueryClientProvider, QueryClient } from 'react-query';
+import {ReactQueryDevtools} from 'react-query/devtools'
+
+import {getTodos, Todo} from './lib/api'
+
+function TodoApp() {
+  const {data: todos} = useQuery<Todo[]>('todos', getTodos, {
+    initialData: [],
+  })
+  return (
+    <div className="App">
+      {JSON.stringify(todos)}
+    </div>
+  );
+}
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <QueryClientProvider client={queryClient}>
+      <TodoApp/>
+      <ReactQueryDevtools/>
+    </QueryClientProvider>
+  )
 }
 
 export default App;
